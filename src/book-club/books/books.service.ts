547049b68e4f0bb43f6df8871213455b2109a4c9
Alexandre <alebonfinela@gmail.com>
2021-08-15 11:53:21 +0200

    Update books.service.ts
    - Added all tasks;
    - base for test;

--- src/book-club/books/books.service.ts
+++ src/book-club/books/books.service.ts
@@ -2,8 +2,14 @@ import { Book, Priority, Suggestion } from './books.types'
 import { getBooks } from './books.store'
 
 import { User } from '../users/users.types'
-import { getUsersFromClub } from '../users/users.service'
+import { getMany as allUsers, getUsersFromClub } from '../users/users.service'
 
+// Each matching criteria should sort the books by Publication Year.
+const sortByYear = (books: Book[]): Book[] => {
+  return books.sort((a, b) => a.publicationYear - b.publicationYear).slice(0, 3) // remove 3 first items.
+}
+
+// The entire list should not contain any duplicate entries.
 const priority = (id: number): Priority => {
   const authorIds = [].concat(...getUsersFromClub(id).map((x => x.authorIds)))
   const genres = [].concat(...getUsersFromClub(id).map((x => x.genres)))
@@ -19,16 +25,7 @@ const priority = (id: number): Priority => {
   }
 }
 
-// b. Each matching criteria should sort the books by Publication Year.
-const sortByYear = (books: Book[]): Book[] => {
-  return books.sort((a, b) => a.publicationYear - b.publicationYear).slice(0, 3) // remove 3 first items
-}
-
-const getMany = (): Book[] => {
-  return getBooks()
-}
-
-// d. The list should display the Title, Author (formatted to {firstName} {lastName}) and the matching Genre(s) of each Book.
+// The list should display the Title, Author (formatted to {firstName} {lastName}) and the matching Genre(s) of each Book.
 const formattedResponse = (books: Book[]): Suggestion[] => {
   const formatted: Suggestion[] = []
 
@@ -46,9 +43,30 @@ const formattedResponse = (books: Book[]): Suggestion[] => {
   return formatted
 }
 
-const getSuggestions =  (user: User, clubId: number): any => {
-  let result: Suggestion[] = []
+// Books most popular by all Users.
+const mostPopularByUsers = (data: any[]): any => {
+  const counted = data.reduce((acc, curr) => {
+    if (curr in acc) {
+        acc[curr]++
+    } else {
+        acc[curr] = 1
+    }
 
+    return acc;
+  }, {});
+
+  const popular = Object.keys(counted).reduce((a, b) => counted[a] > counted[b] ? a : b);
+
+  // return popular;
+
+  return counted
+}
+
+const getMany = (): Book[] => {
+  return getBooks()
+}
+
+const getSuggestions =  (user: User, clubId: number): Suggestion[] => {
   const books = getMany()
   const { authorIds, genres: userGenres } = user
 
@@ -56,13 +74,54 @@ const getSuggestions =  (user: User, clubId: number): any => {
   * Books 1-3: Matching all shared Authors (prioritized by number of users sharing the same Author) of all Users in the Club.
   *   If none are shared, fallback to most popular by all Users.
   */
-  const booksFromAuthorPriority = sortByYear(books.filter(({ authorId }) => (priority(clubId).authorIds).includes(authorId)))
+  const booksFromAuthorsPriority = sortByYear(books.filter(({ authorId }) => (priority(clubId).authorIds).includes(authorId)))
 
-  result = [...formattedResponse(booksFromAuthorPriority)]
+  // @todo : return only one or list of popular in case of === result
+  // if (booksFromAuthorsPriority.length <= 0) {
+  //   const authors = [].concat(...allUsers().map((x => x.authorIds)))
 
-  return result
+  //   mostPopularByUsers(authors)
+  // }
+
+  /**
+  * Books 4-6: Matching all shared Genres (prioritized by number of users sharing the same Genres) of all Users in the Club.
+  *   If none are shared, fallback to most popular by all Users.
+  */
+  const booksFromGenresPriority = sortByYear(books.filter(({ genres }) => genres.some(genre => priority(clubId).genres.includes(genre))))
+
+  // @todo : return only one or list of popular in case of === result
+  // if (booksFromAuthorsPriority.length <= 0) {
+  //   const authors = [].concat(...allUsers().map((x => x.authorIds)))
+
+  //   mostPopularByUsers(authors)
+  // }
+
+  /**
+  * Books 7-9: Matching all Authors of the specified User.
+  */
+  const authorFromUser = sortByYear(books.filter(({ authorId }) => authorIds.includes(authorId)))
+
+  /**
+  * Books 10-12: Matching all Genres of the specified User.
+  // */
+  const authorFromGenres = sortByYear(books.filter(({ genres }) => genres.some(genre => userGenres.includes(genre))))
+
+
+  /**
+  * Books 13-15: Matching all Authors of the Club’s User that are not shared between all of the Club’s Users.
+  */
+  const booksFromAuthorsNotShared = sortByYear(books.filter(({ authorId }) => !(priority(clubId).authorIds).includes(authorId)))
+
+  return [
+    ...formattedResponse(booksFromAuthorsPriority),
+    ...formattedResponse(booksFromGenresPriority),
+    ...formattedResponse(authorFromUser),
+    ...formattedResponse(authorFromGenres),
+    ...formattedResponse(booksFromAuthorsNotShared)
+  ]
 }
 
-export { getSuggestions }
+export { getSuggestions, getMany }
 
+export const exportedForTesting = { sortByYear, priority, formattedResponse }
 
